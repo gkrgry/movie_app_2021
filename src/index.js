d4fbb88d258414c47f9002c0542d95fffee71369
Ktg <gkrgry@gmail.com>
2021-09-15 15:22:37 +0900

    jsx 공부 및 Potato.js 삭제

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-    <App /> ,document.getElementById('Potato')
+    <App />,document.getElementById('Potato')
+    
     
 );
 
