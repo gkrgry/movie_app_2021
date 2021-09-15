b3ed393200002224a7d8f3391e8d98eebace92f0
Ktg <gkrgry@gmail.com>
2021-09-15 16:43:48 +0900

    map 함수 , 화살표 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-const foodLink = [
+const foodLike = [
   {
     name: "chikin",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU2VIPw7JbEM8zeFPd9Llveo8Io2jINQoiHg&usqp=CAU"
@@ -12,21 +12,34 @@ const foodLink = [
 
 ]
 
+/*function renderFood(dish){
+  return (
+    <Food name={dish.name} image={dish.image} />
+  )
+}*/
+
 function App() {
+  // console.log(foodLike.map(renderFood));
   return (
     <div className="App">
-      Hello React!!!!! 
-      <Food fav="kimchi" something = {true} papapa ={['hello', 1, 2, true]} />
-      <Food fav="a" />
-      <Food fav="b" />
+      {
+        // foodLike.map(renderFood)
+
+        foodLike.map(dish => (<Food name={dish.name} image={dish.image} />))
+        
+      }
     </div>
   );
 }
 
 function Food(props){
   // console.log(props);
-  const { fav} = props
-  return <h1>I like {fav}</h1>
+  // const { fav} = name
+  return  (<div>
+  <h1>I like {props.name} </h1> 
+  <img src={props.image} />
+  </div>)
+  
 
   // return <h1>I like {props.fav}</h1>
 }
