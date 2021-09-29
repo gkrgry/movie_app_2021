773ae5993bb40ef1a101f0e17f13d73fb519ea46
Ktg <gkrgry@gmail.com>
2021-09-29 16:49:46 +0900

    constructor -> render -> componentDidMount 순서로 생명주기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class App extends React.Component{
+    constructor(props){
+        super(props)
+        console.log("constructor");
+    }
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate...Goodbye");
+    }
+    state = {
+        count: 0
+    }
+
+    add = () => {
+        this.setState({count: this.state.count+1})
+    }
+
+    minus = () => {
+        this.setState({count: this.state.count-1})   
+    }
+
   render(){
+      console.log("render");
     return (
-      <h1>hello</h1>
+        <div>
+            
+        <h1>The number is: {this.state.count} </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+        </div> 
     )
   }
 }
