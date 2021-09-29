726b4f7d853c26cb00d95405a268a9d559e0a9f8
Ktg <gkrgry@gmail.com>
2021-09-29 15:43:44 +0900

    기존 app.js 를 app1.js 로 변경한 후 새로운 app.js 생성

--- src/App.js
+++ src/App.js
@@ -1,59 +1,10 @@
-import PropType from 'prop-types'
+import React from 'react';
 
-const foodLike = [
-  {
-    id: 1,
-    name: "dosirack",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU2VIPw7JbEM8zeFPd9Llveo8Io2jINQoiHg&usqp=CAU",
-    alt: '도시락',
-    rating: 5.0
-  },
-  {
-    id: 2,
-    name: "lice",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqZZWRIyHxRCTzh93aDrt7inRRh9tRlKYGWw&usqp=CAU",
-    alt: 'lice',
-    rating: 4.5
+class App extends React.Component{
+  render(){
+    return (
+      <h1>hello</h1>
+    )
   }
-
-]
-
-/*function renderFood(dish){
-  return (
-    <Food name={dish.name} image={dish.image} />
-  )
-}*/
-
-function App() {
-  // console.log(foodLike.map(renderFood));
-  return (
-    <div className="App">
-      {
-        // foodLike.map(renderFood)
-
-        foodLike.map(dish => (<Food key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          alt={dish.alt}
-          rating={dish.rating}
-          />))
-        
-      }
-    </div>
-  );
 }
-
-function Food({name, picture}/*props*/){
-  // console.log(props);
-  // const { fav} = name
-  return  (<div>
-  <h1>I like {name} </h1> 
-  <img src={picture} />
-  </div>)
-  
-
-  // return <h1>I like {props.fav}</h1>
-}
-
 export default App;
-
