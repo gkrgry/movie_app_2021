d3ff8b38ab49c58c969287b42b41d247d7b183e4
Ktg <gkrgry@gmail.com>
2021-10-13 16:54:48 +0900

    movie.js 파일에 내용 가져와서 뿌려주기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import axios from "axios"
+import Movie from "./Movie"
+import "./App.css"
 
 class App extends React.Component{
   state = {
@@ -14,21 +16,44 @@ class App extends React.Component{
                 movies
             }
         }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json")
-    // const movies 
-    console.log(movies);
-  }
+      } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating")
+      this.setState({movies, isLoading:false})
+      console.log(movies);
+    }
 
   componentDidMount(){
       this.getMovies()
   }
 
   render() {
-    const {isLoading} = this.state
+    const {isLoading, movies} = this.state
     return(
-      <div>
-        { isLoading ? 'Loading....' : '영화 데이터 출력' }
-        </div>
+      <section className='container'>
+        { isLoading ?(
+          <div className='loader'>
+            <span className='loader-text'>Loading...</span>
+          </div>
+        ) : (
+          <div className='movies'>
+            {
+               movies.map((movie) =>{
+                console.log(movie);
+                return( 
+                <Movie
+                  key = {movie.id}
+                  id = {movie.id}
+                  year = {movie.year}
+                  title = {movie.title}
+                  summary = {movie.summary}
+                  poster = {movie.medium_cover_image}
+                  genres = {movie.genres}
+                  />
+                )})
+            }
+          </div>
+        ) 
+        }
+        </section>
     )
   }
 
