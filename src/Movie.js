dcf6f2e6caf8c5dd5e48605e613310c1d4da3c73
Ktg <gkrgry@gmail.com>
2021-10-27 15:08:41 +0900

    영화 장르 배열로 받는거 영화소개 글자수 제한

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,14 @@ function Movie({title ,year ,summary ,poster, genres}) {
          <div className='movie-data'>
              <h3 className='movie-title'>{title}</h3>
              <h5 className='movie-year'>{year}</h5>
-             <p className='moive-summary'>{summary}</p>
+             <ul className='movie-genres'>
+                {genres.map((genre,index) => {
+                    return(
+                    <li key={index} className='movie-genre'>{genre}</li>
+                    )
+                })}
+             </ul>
+             <p className='moive-summary'>{summary.slice(0,180)}...</p>
              
          </div>
         </div>
