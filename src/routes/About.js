da61ff34e5429b77eb23e6874a00a5901f33629d
Ktg <gkrgry@gmail.com>
2021-10-27 16:47:34 +0900

    라우터 하는법

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import './About.css';
+
+function About(){
+    return(
+        <div className="about__container">
+        <span>
+            <h1>Hello About!</h1>
+        </span>
+        </div>
+    )
+}
+export default About
