7f06f34ef9252e2f5a94fb658e37b44a49a60034
Ktg <gkrgry@gmail.com>
2021-11-03 16:27:52 +0900

    Detail.js 리다이렉트

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,20 @@
+import React from "react"
 
 
-function Detail(props){
-    console.log(props);
-    return(
-        <span>
+class Detail extends React.Component{
+    componentDidMount(){
+        const{ location, history } = this.props
+        if(location.state === undefined){
+            history.push('/')
+        }
+    }
+    render(){
+        return(
+            <span>
             hello
         </span>
-    )
+        )
+    }
 }
 
 export default Detail
