532f146b5a68751b007ac0c3c54207bec2be9e53
Ktg <gkrgry@gmail.com>
2021-11-03 15:31:09 +0900

    app.js에 navigation 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css"
 import { HashRouter, Route} from 'react-router-dom'
 import About from './routes/About'
 import Home from "./routes/Home"
+import Navigation from './components/Navigation.js'
 
 function App() {
   return(
     <HashRouter>
+      <Navigation/>
       <Route path='/' exact={true} component={Home}>
       </Route>
       <Route path='/about' component={About}>
